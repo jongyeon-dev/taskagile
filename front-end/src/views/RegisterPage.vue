402b7715b26e00785c7d55a7c5e40be669a12299
JongYeon Kim <jongyeon.kim@daum.net>
2021-01-31 18:02:19 +0900

    add validation on front-end

--- front-end/src/views/RegisterPage.vue
+++ front-end/src/views/RegisterPage.vue
@@ -8,19 +8,37 @@
         </div>
         <form @submit.prevent="submitForm">
           <div v-show="errorMessage" class="alert alert-danger failed">{{ errorMessage }}</div>
-         <div class="form-group">
-          <label for="username">Username</label>
-          <input type="text" class="form-control" id="username" v-model="form.username">
-         </div>
-         <div class="form-group">
-           <label for="emailAddress">Email address</label>
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" id="username" v-model="form.username">
+            <div class="field-error" v-if="$v.form.username.$dirty">
+              <div class="error" v-if="!$v.form.username.required">Username is required</div>
+              <div class="error" v-if="!$v.form.username.alphaNum">Username can only contain letters and numbers</div>
+              <div class="error" v-if="!$v.form.username.minLength">Username must have at least {{$v.form.username.$params.minLength.min}} letters.</div>
+              <div class="error" v-if="!$v.form.username.maxLength">Username is too long. It can contains maximium {{$v.form.username.$params.maxLength.max}} letters.</div>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="emailAddress">Email address</label>
             <input type="email" class="form-control" id="emailAddress" v-model="form.emailAddress">
-         </div>
-         <div class="form-group">
-           <label for="password">Password</label>
+            <div class="field-error" v-if="$v.form.emailAddress.$dirty">
+              <div class="error" v-if="!$v.form.emailAddress.required">Email address is required</div>
+              <div class="error" v-if="!$v.form.emailAddress.email">This is not a valid email address</div>
+              <div class="error" v-if="!$v.form.emailAddress.maxLength">Email address is too long. It can contains maximium {{$v.form.emailAddress.$params.maxLength.max}} letters.</div>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
             <input type="password" class="form-control" id="password" v-model="form.password">
-         </div>
-         <button type="submit" class="btn btn-primary brn-block">Create account</button>
+            <div class="field-error" v-if="$v.form.password.$dirty">
+              <div class="error" v-if="!$v.form.password.required">Password is required</div>
+              <div class="error" v-if="!$v.form.password.minLength">Password is too short. It can contains at least {{$v.form.password.$params.minLength.min}} letters.</div>
+              <div class="error" v-if="!$v.form.password.maxLength">Password is too long. It can contains maximium {{$v.form.password.$params.maxLength.max}} letters.</div>
+            </div>
+          </div>
+          <button type="submit" class="btn btn-primary btn-block">Create account</button>
+          <p class="accept-terms text-muted">By clicking “Create account”, you agree to our <a href="#">terms of service</a> and <a href="#">privacy policy</a>.</p>
+          <p class="text-center text-muted">Already have an account? <a href="/login">Sign in</a></p>
         </form>
       </div>
     </div>
@@ -37,6 +55,7 @@
 </template>
 
 <script>
+import { required, email, minLength, maxLength, alphaNum } from 'vuelidate/lib/validators'
 import registrationService from '@/services/registration'
 
 export default {
@@ -51,9 +70,33 @@ export default {
       errorMessage: ''
     }
   },
+  validations: {
+    form: {
+      username: {
+        required,
+        minLength: minLength(2),
+        maxLength: maxLength(50),
+        alphaNum
+      },
+      emailAddress: {
+        required,
+        email,
+        maxLength: maxLength(100)
+      },
+      password: {
+        required,
+        minLength: minLength(6),
+        maxLength: maxLength(30)
+      }
+    }
+  },
   methods: {
     submitForm () {
-      // Todo : 데이터 검증하기
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        return
+      }
+
       registrationService.register(this.form).then(() => {
         this.$router.push({name: 'LoginPage'})
       }).catch((error) => {
@@ -63,8 +106,9 @@ export default {
   }
 }
 </script>
+
 <style lang="scss" scoped>
-  .container {
+.container {
   max-width: 900px;
 }
 
@@ -77,6 +121,27 @@ export default {
   text-align: center;
   margin-bottom: 40px;
 
+  .tagline {
+    line-height: 180%;
+    color: #666;
+  }
+
+ .logo {
+    max-width: 150px;
+    margin: 0 auto;
+  }
+}
+
+.register-form {
+
+  .form-group label {
+    font-weight: bold;
+    color: #555;
+  }
+
+  .accept-terms {
+    margin: 20px 0 40px 0;
+  }
 }
 
 .footer {
